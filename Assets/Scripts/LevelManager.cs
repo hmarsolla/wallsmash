8f08b9a60fe65aeb486aebfd42327f0af2bfb30a
Heitor <hmarsolla@hotmail.com>
2017-10-29 21:51:47 -0200

    First Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string levelname)
+	{
+		SceneManager.LoadScene(levelname);
+	}
+
+	public void QuitRequest ()
+	{
+		Application.Quit();
+	}
+}
